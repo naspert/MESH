6b41ed43b3c6baf13449cbbffc104d4719fa92a9
Nicolas Aspert <nicolas.aspert@epfl.ch>
2001-03-27 08:15:06 +0000

    Added the prototype of the 'dist' function

--- lib3d/include/geomutils.h
+++ lib3d/include/geomutils.h
@@ -1,4 +1,4 @@
-/* $Id: geomutils.h,v 1.2 2001/03/26 14:26:29 aspert Exp $ */
+/* $Id: geomutils.h,v 1.3 2001/03/27 08:15:06 aspert Exp $ */
 #include <3dmodel.h>
 
 #ifndef _GEOMUTILS_PROTO
@@ -9,7 +9,7 @@ vertex crossprod(vertex, vertex);
 double cross_product2d(vertex, vertex, vertex);
 double scalprod(vertex, vertex);
 double norm(vertex);
-double distance(vertex, vertex);
+double dist(vertex, vertex);
 void normalize(vertex*);
 int inside(vertex, vertex, double);
 void compute_circle2d(vertex, vertex, vertex, double*, vertex*);
