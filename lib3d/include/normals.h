c5480c8c7d2b6230e8a31d4ffff1fcfcd65ec5ce
Nicolas Aspert <nicolas.aspert@epfl.ch>
2001-04-02 13:55:54 +0000

    Fixed incorrect prototype for 'bfs_build_spanning_tree' (probably harmless)

--- lib3d/include/normals.h
+++ lib3d/include/normals.h
@@ -1,4 +1,4 @@
-/* $Id: normals.h,v 1.1 2001/03/12 14:50:32 aspert Exp $ */
+/* $Id: normals.h,v 1.2 2001/04/02 13:55:54 aspert Exp $ */
 #include <3dmodel.h>
 
 #ifndef _NORMALS_PROTO
@@ -12,7 +12,7 @@ int test_cycle(face_tree_ptr, int);
 edge_list_ptr add_edge(edge_list_ptr, int, int, int, int);
 int find_adj_edges(model*, int, edge_list_ptr);
 void destroy_tree(face_tree_ptr);
-face_tree_ptr bfs_build_spanning_tree(model*);
+face_tree_ptr* bfs_build_spanning_tree(model*);
 int find_center(face*, int, int);
 void swap_vert(edge_v*);
 void update_child_edges(face_tree_ptr, int, int, int);
