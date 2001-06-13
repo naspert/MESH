132aeaab02872468b1edb1323c12714a51211c31
Davy Jacquet <Davy.Jacquet@epfl.ch>
2001-06-13 09:48:32 +0000

    *** empty log message ***

--- ScreenWidget.h
+++ ScreenWidget.h
@@ -1,7 +1,8 @@
-/* $Id: ScreenWidget.h,v 1.4 2001/06/12 08:53:17 aspert Exp $ */
+/* $Id: ScreenWidget.h,v 1.5 2001/06/13 09:48:32 jacquet Exp $ */
 #ifndef SCREENWIDGET_H
 #define SCREENWIDGET_H
 
+#include <qmessagebox.h>
 #include <qmenubar.h>
 #include <qpopupmenu.h>
 #include <RawWidget.h>
@@ -20,11 +21,16 @@
 #include <qmime.h>
 
 
+
 class ScreenWidget : public QWidget
 {
+  Q_OBJECT
 public:
-  ScreenWidget( model *raw_model1,model *raw_model2,QWidget *parent=0, const char *name=0 );
+  ScreenWidget( model *raw_model1,model *raw_model2,double dmoymin, double dmoymax, QWidget *parent=0, const char *name=0 );
 /*   void keyPressEvent(QKeyEvent *k); */
+
+  public slots:
+    void about();
 
  private:
   QString filename;  
