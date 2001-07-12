981c51bb8734f1ec9711d3b825fe9962898c8c71
Nicolas Aspert <nicolas.aspert@epfl.ch>
2001-07-12 08:40:32 +0000

    Optimizations in the 'liste' function (now using 'qsort' to find which cells are intersecting the face)

--- compute_error.c
+++ compute_error.c
@@ -1,23 +1,23 @@
-/* $Id: compute_error.c,v 1.12 2001/07/11 08:06:08 jacquet Exp $ */
+/* $Id: compute_error.c,v 1.13 2001/07/12 08:40:32 aspert Exp $ */
 
 #include <compute_error.h>
 
-#define E 0.0000001
+#define E 1e-10
 
 
 
 /* computes the distance between a point and a plan defined by 3 points */
 double distance(vertex point,vertex A,vertex normal)
 {
-double k;
-double dist;
+  double k;
+  double dist;
+  
+  k = scalprod(A,normal);
 
-k=-(normal.x*A.x+normal.y*A.y+normal.z*A.z);
+  dist=fabs(scalprod(point, normal) + k);
 
-dist=fabs(-normal.x*point.x-normal.y*point.y-normal.z*point.z-k);
-
-return dist;
-
+  return dist;
+  
 }
 
 /***************************************************************************/
@@ -65,35 +65,35 @@ return(erreur);
 /*****************************************************************************/
 double dist_pt_surf(vertex A,vertex B,vertex C,vertex point,vertex normal)
 {
-double k,tmp;
-int i=0;
-double d,dmin=200;
-vertex M,AM,AB,AC,BC,AP,BP,PM,CP;
-double ah,d1,d2,d3,ab,ac,bc,ap,bp,cp,am,ap_ac,ap_ab,bp_bc;
-double yint;
-double alpha,beta,Xp,Yp;
+  double k,tmp;
+  int i=0;
+  double d, dmin=FLT_MAX;
+  vertex M,AM,AB,AC,BC,AP,BP,PM,CP;
+  double ah,d1,d2,d3,ab,ac,bc,ap,bp,cp,am,ap_ac,ap_ab,bp_bc;
+  double yint;
+  double alpha,beta,Xp,Yp;
+  
+  AB.x=B.x-A.x;
+  AB.y=B.y-A.y;
+  AB.z=B.z-A.z;   
+  
+  AC.x=C.x-A.x;
+  AC.y=C.y-A.y;
+  AC.z=C.z-A.z;
+  
+  BC.x=C.x-B.x;
+  BC.y=C.y-B.y;
+  BC.z=C.z-B.z;
+  
+  
+  k=-scalprod(normal,A);
+  
+  tmp=scalprod(normal,point)+k;
+  M.x=point.x-normal.x*tmp;
+  M.y=point.y-normal.y*tmp;
+  M.z=point.z-normal.z*tmp;
+  
 
-   AB.x=B.x-A.x;
-   AB.y=B.y-A.y;
-   AB.z=B.z-A.z;   
-   
-   AC.x=C.x-A.x;
-   AC.y=C.y-A.y;
-   AC.z=C.z-A.z;
-   
-   BC.x=C.x-B.x;
-   BC.y=C.y-B.y;
-   BC.z=C.z-B.z;
-
-
-k=-scalprod(normal,A);
-/* k=-(normal.x*A.x+normal.y*A.y+normal.z*A.z); */
-tmp=scalprod(normal,point)+k;
-M.x=point.x-normal.x*tmp;
-M.y=point.y-normal.y*tmp;
-M.z=point.z-normal.z*tmp;
-
-/* printf("%lf %lf %lf\n",M.x,M.y,M.z); */
 
 
  if(M.x<=max(A.x,B.x) && M.x>=min(A.x,B.x)){
@@ -120,7 +120,7 @@ M.z=point.z-normal.z*tmp;
 
 
 
- if(i==1||i==-1) {
+ if(i==1) {
    PM.x=point.x-M.x;
    PM.y=point.y-M.y;
    PM.z=point.z-M.z;
@@ -149,21 +149,21 @@ M.z=point.z-normal.z*tmp;
 
    ap_ab=scalprod(AP,AB);
    if(ap_ab>0 && ap_ab<ab)
-     d1=ap-(ap_ab*ap_ab)/ab;
+     d1 = ap - (ap_ab*ap_ab)/ab;
    else
-     d1=min(ap,bp);
+     d1 = min(ap,bp);
 
-   ap_ac=scalprod(AP,AC);
+   ap_ac = scalprod(AP,AC);
    if(ap_ac>0 && ap_ac<ac)
-     d2=ap-(ap_ac*ap_ac)/ac;
+     d2 = ap - (ap_ac*ap_ac)/ac;
    else
-     d2=min(ap,cp);
+     d2 = min(ap,cp);
 
-   bp_bc=scalprod(BP,BC);
+   bp_bc = scalprod(BP,BC);
    if(bp_bc>0 && bp_bc<bc)
-     d3=bp-(bp_bc*bp_bc)/bc;
+     d3 = bp - (bp_bc*bp_bc)/bc;
    else
-     d3=min(bp,cp);
+     d3 = min(bp,cp);
 
    dmin=min3(d1,d2,d3);
  }
@@ -177,14 +177,13 @@ else
 /*****************************************************************************/
 /*      compute the distance between a point and a cell                      */
 /*****************************************************************************/
-double dist_pt_cellule(vertex point, int m, int n, int o, int a, int b, int c, double ccube)
-{
-double d;
-vertex sommet;
+double dist_pt_cellule(vertex point, int m, int n, int o, int a, int b, int c, 
+		       double ccube) {
+  double d;
+  vertex sommet;
 
   if(a==m && b==n && c==o)
     return -1;
-
   else if(a==m && b==n){
     if(c>o)
       return (c*ccube-point.z);
@@ -379,9 +378,10 @@ sample* echantillon(vertex a, vertex b, vertex c,double k)
   sample *sample1;
 
   if((sample1=(sample*)malloc(sizeof(sample)))==NULL){
-  printf("impossible d'allouer de la memoire");
-  exit(-1);
+    printf("impossible d'allouer de la memoire");
+    exit(-1);
   }
+  sample1->sample = NULL;
 
   l1.x=b.x-a.x;
   l1.y=b.y-a.y;
@@ -391,22 +391,18 @@ sample* echantillon(vertex a, vertex b, vertex c,double k)
   l2.y=c.y-a.y;
   l2.z=c.z-a.z;
 
-  nbsamples=(1+1/k)*(1+1/k)/2+1+1/k; 
+  nbsamples=(1 + 1/k)*(1 + 1/k)/2 + 1 + 1/k; 
    
-  if((sample1->sample=(vertex*)malloc(sizeof(vertex)))==NULL){
-    printf("impossible d'allouer de la memoire");
-    exit(-1);
-  }
-  for (i=0;i<1.000001;i+=k) {
-    for (j=0;j<1.000001;j+=k) {
-      if (i+j<1.000001) {
-	if(h>0)
-	  sample1->sample=(vertex*)realloc(sample1->sample,(h+1)*sizeof(vertex));
+
+  for (i=0; i<1+E; i+=k) {
+    for (j=0; j<1+E; j+=k) {
+      if (i+j<1+E) {
+	sample1->sample=(vertex*)realloc(sample1->sample,(h+1)*sizeof(vertex));
         sample1->sample[h].x=a.x+i*l1.x+j*l2.x;
         sample1->sample[h].y=a.y+i*l1.y+j*l2.y;
-        sample1->sample[h].z=a.z+i*l1.z+j*l2.z;
-        h++;
-	}
+        sample1->sample[h++].z=a.z+i*l1.z+j*l2.z;
+
+      }
     }
   }
   sample1->nbsamples=h;
@@ -415,89 +411,114 @@ sample* echantillon(vertex a, vertex b, vertex c,double k)
 
 }
 
+
+int compar_cpe(const void *face0, const void *face1) {
+  int *f0, *f1;
+
+  f0 = (int*)face0;
+  f1 = (int*)face1;
+
+  if (*f0 == *f1)
+    return 0;
+  else if (*f0 < *f1)
+    return -1;
+  else
+    return 1;
+}
+
 /****************************************************************************/
 /* fonction qui repertorie pour chaque face les cellules avec lesquelles    */
 /*     elle a une intersection                                              */
 /****************************************************************************/
 cellules* liste(model *raw_model,double samplethin,vertex grille,double ccube,vertex bbox0,vertex bbox1)
 {
-cellules *cell;
-int h,i,j,k,m,n,o,cellule,state=0;
-sample *sample1;
-vertex A,B,C;
+  cellules *cell;
+  int h,i,j,k,m,n,o,cellule, cellule_old,state=0;
+  int size_cell;
+  sample *sample1;
+  vertex A,B,C;
+  int *tmp;
+  /* bbox0=raw_model->bBox[0]; */
+  /* bbox1=raw_model->bBox[1]; */
+  
 
-/* bbox0=raw_model->bBox[0]; */
-/* bbox1=raw_model->bBox[1]; */
-
-
-raw_model->face_normals=(vertex*)malloc(raw_model->num_faces*sizeof(vertex));
-cell=(cellules *)malloc((raw_model->num_faces)*sizeof(cellules));
-
+  raw_model->face_normals=(vertex*)malloc(raw_model->num_faces*sizeof(vertex));
+  cell=(cellules *)malloc((raw_model->num_faces)*sizeof(cellules));
+  
  for(i=0;i<raw_model->num_faces;i++){
-   h=0;
-   cell[i].cube=(int *)malloc(sizeof(int));   
-
+   h = 0;
+   tmp = NULL;
+   cell[i].cube = NULL;
    A=raw_model->vertices[raw_model->faces[i].f0];
    B=raw_model->vertices[raw_model->faces[i].f1];
    C=raw_model->vertices[raw_model->faces[i].f2];
 
-   raw_model->face_normals[i]=ncrossp(A,B,C);
-   sample1=echantillon(A,B,C,0.05);
-
+   raw_model->face_normals[i] = ncrossp(A,B,C);
+   sample1=echantillon(A,B,C,0.1);
+   cellule_old = -1;
    for(j=0;j<sample1->nbsamples;j++){
      state=0;
+     
      m=(int)floor((sample1->sample[j].x-bbox0.x)/ccube);
      n=(int)floor((sample1->sample[j].y-bbox0.y)/ccube);
      o=(int)floor((sample1->sample[j].z-bbox0.z)/ccube);
      
-     if(m==(int)grille.x)
-       m=(int)grille.x-1;
-     if(n==(int)grille.y)
-       n=(int)grille.y-1;
-     if(o==(int)grille.z)
-       o=(int)grille.z-1;
-     if(m==-1)
-       m=0;
-     if(n==-1)
-       n=0;
-     if(o==-1)
-       o=0;
+     if(m == (int)grille.x)
+       m = (int)grille.x - 1;
+     else if (m == -1)
+       m = 0;
 
-     cellule=m+n*grille.x+o*grille.y*grille.x;    
+     if(n == (int)grille.y)
+       n = (int)grille.y - 1;
+     else if (n == -1)
+       n = 0;
 
-     for(k=0; k<h; k++){
-       if(cellule==cell[i].cube[k]){
-         state=1;
-         break;
-       }
+     if(o == (int)grille.z)
+       o = (int)grille.z - 1;
+     else if (o == -1)
+       o = 0;
+
+
+     cellule = m + n*grille.x + o*grille.y*grille.x;    
+
+     if (cellule != cellule_old) {
+       tmp = (int *)realloc(tmp, (h+1)*sizeof(int));
+       tmp[h++] = cellule;
+       cellule_old = cellule;
      }
-     if(state==0){
-       if(h>0){
-         if((cell[i].cube=(int *)realloc(cell[i].cube,(h+1)*sizeof(int)))==NULL){
-           printf("erreur d'allocation memoire");
-           exit(-1);
-         }
-       }
-       cell[i].cube[h]=cellule;
-       h++;
-     } 
    }
-   if(sample1->sample != NULL)
-     free(sample1->sample);
-   if(sample1 != NULL)
+
+   qsort(tmp, h, sizeof(int), compar_cpe);
+
+   cellule_old = -1;
+   size_cell = 0;
+   for (j=0; j<h; j++) {
+     if (tmp[j] != cellule_old) {
+       cell[i].cube = (int*)realloc(cell[i].cube, (size_cell+1)*sizeof(int));
+       cell[i].cube[size_cell++] = tmp[j];
+       cellule_old = tmp[j];
+
+     }
+   }
+   cell[i].nbcube = size_cell;
+   free(tmp);
+   if(sample1 != NULL) {
+     if(sample1->sample != NULL)
+       free(sample1->sample);
      free(sample1);
-   cell[i].nbcube=h;
+   }
+
 
  }
-/*  for(i=0;i<raw_model->num_faces;i++){ */
-/*    printf("face %d",i); */
+/*  for(i=0;i<raw_model->num_faces;i++) */
+/*    printf("nbcell[%d] = %d\n",i, cell[i].nbcube); */
 /*    for(j=0;j<cell[i].nbcube;j++){ */
 /*      printf(" %d",cell[i].cube[j]); */
 /*    } */
 /*    printf("\n"); */
 /*    } */
  
-return cell;
+ return cell;
 }
 
 /*****************************************************************************/
@@ -508,169 +529,141 @@ return cell;
 int** cublist(cellules *cell,model *raw_model,vertex grille)
 {
 
-int **tab,i,j,k;
-int *mem;
+  int **tab,i,j,k;
+  int *mem;
 
-mem=(int*)calloc((int)(grille.x*grille.y*grille.z),sizeof(int));
-tab=(int **)malloc((int)(grille.x*grille.y*grille.z)*sizeof(int*));
+  mem=(int*)calloc((int)(grille.x*grille.y*grille.z),sizeof(int));
+  tab=(int **)malloc((int)(grille.x*grille.y*grille.z)*sizeof(int*));
 
- for(j=0;j<raw_model->num_faces;j++){
-   for(k=0;k<cell[j].nbcube;k++){
-     i=cell[j].cube[k];
-     if(mem[i]==0)
-       tab[i]=NULL;
-     tab[i]=(int *)realloc(tab[i],(mem[i]+1)*sizeof(int));
-     tab[i][mem[i]]=j;
-     mem[i]++;
-   }
- }
-
- for(i=0;i<(int)grille.x*grille.y*grille.z;i++){
-   if(mem[i]==0)
-     tab[i]=NULL;
-   tab[i]=(int *)realloc(tab[i],(mem[i]+1)*sizeof(int));
+  for(j=0;j<raw_model->num_faces;j++){
+    for(k=0;k<cell[j].nbcube;k++){
+      i=cell[j].cube[k];
+      if(mem[i]==0)
+	tab[i]=NULL;
+      tab[i]=(int *)realloc(tab[i],(mem[i]+1)*sizeof(int));
+      tab[i][mem[i]]=j;
+      mem[i]++;
+    }
+  }
+  
+  for(i=0;i<(int)grille.x*grille.y*grille.z;i++){
+    if(mem[i]==0)
+      tab[i]=NULL;
+    tab[i]=(int *)realloc(tab[i],(mem[i]+1)*sizeof(int));
    tab[i][mem[i]]=-1;
- }
-
-return(tab);
+  }
+  
+  return(tab);
 }
 /****************************************************************************/
 /*     fonction qui calcule la distance d'un point a une surface            */
 /****************************************************************************/
-double pcd(vertex point,model *raw_model,int **repface,vertex grille,double ccube,vertex bbox0,vertex bbox1)
-{
-int m,n,o;
-int mmem,nmem,omem;
-int a,b,c;
-int face,cell;
-vertex /*bbox0,bbox1,*/A,B,C,normal,test;
-int cellule;
-int i,j=0,k=0,l=0;
-double dist,dmin=201;
-int *memoire,*memoire2;
-int rang=0,rang2=0;
-int state=0,state2=0;
+double pcd(vertex point,model *raw_model,int **repface,vertex grille,double ccube,vertex bbox0,vertex bbox1) {
+  int m, n, o;
+  int mmem, nmem, omem;
+  int a, b, c;
+  int face, cell;
+  vertex A, B, C, normal, test;
+  int cellule;
+  int i, j=0, k=0, l=0;
+  double dist, dmin=FLT_MAX;
+  int *memoire=NULL, *memoire2=NULL;
+  int rang=0, rang2=0;
+  int state=0, state2=0;
 
 
-/* int x,y,z; */
-/* double dtest=0; */
-/* vertex X,Y,Z; */
-
-memoire=NULL;
-memoire2=NULL;
-
-/* bbox0=raw_model->bBox[0]; */
-/* bbox1=raw_model->bBox[1]; */
-
-test.x=point.x-bbox0.x;
-test.y=point.y-bbox0.y;
-test.z=point.z-bbox0.z;
 
 
- m=(int)floor((point.x-bbox0.x)/ccube);
- n=(int)floor((point.y-bbox0.y)/ccube);
- o=(int)floor((point.z-bbox0.z)/ccube);
+
+  test.x=point.x-bbox0.x;
+  test.y=point.y-bbox0.y;
+  test.z=point.z-bbox0.z;
+
+
+  m=(int)floor((point.x-bbox0.x)/ccube);
+  n=(int)floor((point.y-bbox0.y)/ccube);
+  o=(int)floor((point.z-bbox0.z)/ccube);
+  
+  if(m==(int)grille.x)
+    m=(int)grille.x-1;
+  if(n==(int)grille.y)
+    n=(int)grille.y-1;
+  if(o==(int)grille.z)
+    o=(int)grille.z-1;
  
- if(m==(int)grille.x)
-   m=(int)grille.x-1;
- if(n==(int)grille.y)
-   n=(int)grille.y-1;
- if(o==(int)grille.z)
-   o=(int)grille.z-1;
- 
- cellule=m+n*grille.x+o*grille.y*grille.x;
-/* printf("cellule: %d\n",cellule); */
-/* printf("%lf %lf %lf\n",point.x,point.y,point.z);  */
-/* printf("%d %d %d\n",m,n,o);  */
+  cellule = m + n*grille.x + o*grille.y*grille.x;
+
 
  /* quelles sont les cellules qu'il faut traiter? */
- while(state==0){
-   for(c=o-k;c<=o+k;c++){ 
-     for(b=n-k;b<=n+k;b++){
-       for(a=m-k;a<=m+k;a++){
-	 cellule=a+b*grille.x+c*grille.y*grille.x;
-	 if(a<grille.x && b<grille.y && c<grille.z && a>0 && b>0 && c>0){
-/* 	 if(cellule>=0 && cellule<grille.x*grille.y*grille.z){ */
-	   if(repface[cellule][0]!=-1){
-	     state=1;
-	   }
-	 }
-       }
-     }
-   }
-   k=k+1;
- }
+  while(state==0){
+    for(c=o-k;c<=o+k;c++){ 
+      for(b=n-k;b<=n+k;b++){
+	for(a=m-k;a<=m+k;a++){
+	  cellule = a + b*grille.x + c*grille.y*grille.x;
+	  if(a<grille.x && b<grille.y && c<grille.z && a>0 && b>0 && c>0 &&
+	     repface[cellule][0]!=-1 ){
+	    state=1;
+	  }
+	}
+      }
+    }
+    k++;
+  }
+  
+  
 
-
-/*  k=k+1; */
-/* printf("k= %d ",k); */
-
- /* on cherche la distance min dans les cellules qu'on doit obligatoirement traiter */
- k=k-1;
- for(c=o-k;c<=o+k;c++){ 
-   for(b=n-k;b<=n+k;b++){
-     for(a=m-k;a<=m+k;a++){
-       j=0;
-       cellule=a+b*grille.x+c*grille.y*grille.x;
-       if(a<grille.x && b<grille.y && c<grille.z && a>0 && b>0 && c>0){
-/*        if(cellule>=0 && cellule<grille.x*grille.y*grille.z){ */
+  /* on cherche la distance min dans les cellules qu'on doit obligatoirement traiter */
+   k--; 
+  for(c=o-k; c<=o+k; c++){ 
+    for(b=n-k; b<=n+k; b++){
+      for(a=m-k; a<=m+k; a++){
+	j=0;
+	cellule = a + b*grille.x + c*grille.y*grille.x;
+	if(a<grille.x && b<grille.y && c<grille.z && a>0 && b>0 && c>0){
 
 	 /* on garde en memoire les cellules deja traitees */
-	 memoire=(int*)realloc(memoire,(rang+1)*sizeof(int));
-	 memoire[rang]=cellule;
-	 rang++;
+	  memoire=(int*)realloc(memoire,(rang+1)*sizeof(int));
+	  memoire[rang++]=cellule;
 	 
-	 while(repface[cellule][j]!=-1){
-	   state2=0;
-	   for(i=0;i<rang2;i++){
-	     if(repface[cellule][j]==memoire2[i]){
-	       state2=1;
-	       break;
-	     }
-	   }
-	   if(state2==0){
-	     memoire2=(int*)realloc(memoire2,(rang2+1)*sizeof(int));
-	     memoire2[rang2]=repface[cellule][j];
-	     rang2++;
-	   
-	     A=raw_model->vertices[raw_model->faces[repface[cellule][j]].f0];
-	     B=raw_model->vertices[raw_model->faces[repface[cellule][j]].f1];
-	     C=raw_model->vertices[raw_model->faces[repface[cellule][j]].f2];
-	   
-	     normal=raw_model->face_normals[repface[cellule][j]];
-	     
-	     
-	     dist=dist_pt_surf(A,B,C,point,normal);
-	     
-	     
-	     if(dist<dmin){
-	       dmin=dist;
-/* 	       cell=cellule; */
-/* 	       face=repface[cellule][j]; */
-/* 	       x=a; */
-/* 	       y=b; */
-/* 	       z=c; */
-/* 	       X=A; */
-/* 	       Y=B; */
-/* 	       Z=C; */
-	     }
-	   }
-	   j++;
-	 }
-       }
-     }
-   }
- }
+	  while(repface[cellule][j]!=-1){
+	    state2=0;
+	    for(i=0;i<rang2;i++){
+	      if(repface[cellule][j]==memoire2[i]){
+		state2 = 1;
+		break;
+	      }
+	    }
+	    if(state2 == 0){
+	      memoire2 = (int*)realloc(memoire2,(rang2+1)*sizeof(int));
+	      memoire2[rang2++] = repface[cellule][j];
+	      
+	      A=raw_model->vertices[raw_model->faces[repface[cellule][j]].f0];
+	      B=raw_model->vertices[raw_model->faces[repface[cellule][j]].f1];
+	      C=raw_model->vertices[raw_model->faces[repface[cellule][j]].f2];
+	      
+	      normal=raw_model->face_normals[repface[cellule][j]];
+	      
+	      dist=dist_pt_surf(A,B,C,point,normal);
+	      
+	      
+	      if(dist<dmin)
+		dmin=dist;
+	    }
+	    j++;
+	  }
+	}
+      }
+    }
+  }
 
  /* on cherche la distance min dans les cellules adjacentes */
- k=k+1;
+ k++;
  for(c=o-k;c<=o+k;c++){ 
    for(b=n-k;b<=n+k;b++){
      for(a=m-k;a<=m+k;a++){
        state=0;
        j=0;
-       cellule=a+b*grille.x+c*grille.y*grille.x;
-/*        if(cellule>=0 && cellule<grille.x*grille.y*grille.z){ */
+       cellule = a + b*grille.x + c*grille.y*grille.x;
        if(cellule>=0 && cellule<grille.x*grille.y*grille.z){
 	 /* on regarde si on a pas deja traite cette cellule */
 	 for(i=0;i<rang;i++){
@@ -681,7 +674,7 @@ test.z=point.z-bbox0.z;
 	 }
 	 
 	 if(state==0 && dist_pt_cellule(test,m,n,o,a,b,c,ccube)<dmin){
-	   while(repface[cellule][j]!=-1){
+	   while(repface[cellule][j] != -1){
 	     state2=0;
 	     for(i=0;i<rang2;i++){
 	       if(repface[cellule][j]==memoire2[i]){
@@ -690,9 +683,8 @@ test.z=point.z-bbox0.z;
 	       }
 	     }
 	     if(state2==0){
-	       memoire2=(int*)realloc(memoire2,(rang2+1)*sizeof(int));
-	       memoire2[rang2]=repface[cellule][j];
-	       rang2++;
+	       memoire2 = (int*)realloc(memoire2,(rang2+1)*sizeof(int));
+	       memoire2[rang2++] = repface[cellule][j];
 	       
 	       A=raw_model->vertices[raw_model->faces[repface[cellule][j]].f0];
 	       B=raw_model->vertices[raw_model->faces[repface[cellule][j]].f1];
@@ -703,18 +695,9 @@ test.z=point.z-bbox0.z;
 	       dist=dist_pt_surf(A,B,C,point,normal);
 	       
 	       
-	       if(dist<dmin){
+	       if(dist<dmin)
 		 dmin=dist;
-/* 		 cell=cellule; */
-/* 		 face=repface[cellule][j]; */
-/* 		 x=a; */
-/* 		 y=b; */
-/* 		 z=c; */
-/* 		 dtest=dist_pt_cellule(test,m,n,o,a,b,c,ccube); */
-/* 		 X=A; */
-/* 		 Y=B; */
-/* 		 Z=C; */
-	       }
+
 	     }
 	     j++;
 	   }
@@ -723,19 +706,11 @@ test.z=point.z-bbox0.z;
      }
    }
  }
-
-free(memoire);
-free(memoire2);
-/* free(memoire2); */
-/* fprintf(f,"%lf ",dmin);  */
-/* printf("face %f %f %f\n",X.x,X.y,X.z); */
-/* printf("face %f %f %f\n",Y.x,Y.y,Y.z); */
-/* printf("face %f %f %f\n",Z.x,Z.y,Z.z); */
-
-/* printf("k= %d dmin=%lf face: %d cellule: %d %d %d %d dtest= %f\n",k,dmin,face,cell,x,y,z,dtest); */
-/* printf("dmin= %lf ",dmin); */
-/* printf("face= %d\n",face);  */
-return dmin;
+ 
+ free(memoire);
+ free(memoire2);
+ 
+ return dmin;
 }
 
 
