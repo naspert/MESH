47d5e127d7592407ce948c2dcef8002cd2321cc3
Diego Santa Cruz <diego.santacruz@epfl.ch>
2001-08-08 14:31:33 +0000

    - Made all local functions static and removed from header. - Moved all local data types to C source file. - Added doc for exported functions.

--- compute_error.h
+++ compute_error.h
@@ -1,6 +1,5 @@
-/* $Id: compute_error.h,v 1.7 2001/08/08 13:18:49 dsanta Exp $ */
+/* $Id: compute_error.h,v 1.8 2001/08/08 14:31:33 dsanta Exp $ */
 #include <3dmodel.h>
-#include <geomutils.h>
 
 #ifndef _COMPUTE_ERROR_PROTO
 #define _COMPUTE_ERROR_PROTO
@@ -16,17 +15,9 @@
 BEGIN_DECL
 #undef BEGIN_DECL
 
-/* A list of samples of a surface in 3D space. */
-struct sample_list {
-  vertex* sample; /* Array of sample 3D coordinates */
-  int n_samples;  /* The number of samples in the array */
-};
-
-/* A list of cells */
-struct cell_list {
-  int *cell;   /* The array of the linear indices of the cells in the list */
-  int n_cells; /* The number of elemnts in the array */
-};
+/* --------------------------------------------------------------------------*
+ *                       Exported data types                                 *
+ * --------------------------------------------------------------------------*/
 
 /* A integer size in 3D */
 struct size3d {
@@ -63,86 +54,19 @@ struct dist_surf_surf_stats {
                           * each direction X,Y,Z */
 };
 
-/* Storage for triangle sample errors. */
-struct triag_sample_error {
-  double **err;      /* Error array with 2D addressing. Sample (i,j) has the
-                      * error stored at err[i][j], where i varies betwen 0 and
-                      * n_samples-1 inclusive and j varies between 0 and
-                      * n_samples-i-1 inclusive. */
-  int n_samples;     /* The number of samples in each triangle direction */
-  double *err_lin;   /* Error array with 1D adressing, which varies from 0 to
-                      * n_samples_tot-1 inclusive. It refers to the same
-                      * location as err, thus any change to err is reflected
-                      * in err_lin and vice-versa. The order in the 1D array
-                      * is all errors for i equal 0 and j from 0 to
-                      * n_samples-1, followed by errors for i equal 1 and j
-                      * from 1 to n_samples-2, and so on. */
-  int n_samples_tot; /* The total number of samples in the triangle */
-};
+/* --------------------------------------------------------------------------*
+ *                       Exported functions                                  *
+ * --------------------------------------------------------------------------*/
 
-/* A list of triangles with their associated information */
-struct triangle_list {
-  struct triangle_info *triangles; /* The triangles */
-  int n_triangles;                 /* The number of triangles */
-  double area;                     /* The total triangle area */
-};
-
-/* A triangle and useful associated information. If a vertex of the triangle
- * has an angle of 90 degrees or more, that vertex is C. That way the
- * projection of C on AB is always inside AB. */
-struct triangle_info {
-  vertex a;            /* The A vertex of the triangle */
-  vertex b;            /* The B vertex of the triangle */
-  vertex c;            /* The C vertex of the triangle. The projection of C
-                        * on AB is always inside the AB segment. */
-  vertex ab;           /* The AB vector */
-  vertex ac;           /* The AC vector */
-  vertex bc;           /* The BC vector */
-  double ab_len_sqr;   /* The square of the length of AB */
-  double ac_len_sqr;   /* The square of the length of AC */
-  double bc_len_sqr;   /* The square of the length of BC */
-  double ab_1_len_sqr; /* One over the square of the length of AB */
-  double ac_1_len_sqr; /* One over the square of the length of AC */
-  double bc_1_len_sqr; /* One over the square of the length of BC */
-  vertex d;            /* The perpendicular projection of C on AB. Always in
-                        * the AB segment. */
-  vertex dc;           /* The DC vector */
-  double dc_1_len_sqr; /* One over the square of the length of DC */
-  double da_1_max_coord; /* One over the max (in absolute value) coordinate of
-                          * DA.  */
-  double db_1_max_coord; /* One over the max (in absolute value) coordinate of
-                          * DB */
-  int da_max_c_idx;    /* The index of the coordinate corresponding to
-                        * da_max_coord: 0 for X, 1 for Y, 2 for Z*/
-  int db_max_c_idx;    /* The index of the coordinate corresponding to
-                        * db_max_coord: 0 for X, 1 for Y, 2 for Z*/
-  vertex normal;       /* The (unit length) normal of the ABC triangle
-                        * (orinted with the right hand rule turning from AB to
-                        * AC). */
-};
-
-struct triangle_list* model_to_triangle_list(const model *m);
-
-void sample_triangle(const vertex *a, const vertex *b, const vertex *c,
-                     int n, struct sample_list* s);
-
-struct cell_list *cells_in_triangles(struct triangle_list *tl,
-                                     struct size3d grid_sz, double cell_sz,
-                                     vertex bbox_min);
-
-int** triangles_in_cells(const struct cell_list *cl,
-                         const struct triangle_list *tl,
-                         const struct size3d grid_sz);
-
-double dist_pt_surf(vertex p, const struct triangle_list *tl,
-                    int **faces_in_cell, struct size3d grid_sz,
-                    double cell_sz, vertex bbox_min);
-
-void error_stat_triag(const struct triag_sample_error *tse,
-                      struct face_error *fe);
-
+/* Returns an array of length m->num_vert with the list of faces incident on
+ * each vertex. */
 struct face_list *faces_of_vertex(model *m);
 
+/* Calculates the distance from model m1 to model m2. The triangles of m1 are
+ * sampled using n_spt samples in each direction. The per face (of m1) error
+ * metrics are returned in a new array (of length m1->num_faces) allocated at
+ * *fe_ptr. The overall distance metrics and other statistics are returned in
+ * stats. If quiet is zero a progress meter is displayed in stdout. */
 void dist_surf_surf(const model *m1, const model *m2, int n_spt,
                     struct face_error *fe_ptr[],
                     struct dist_surf_surf_stats *stats, int quiet);
