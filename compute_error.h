1fa203b2dc91fa26f37e111655694eafb142c46a
Diego Santa Cruz <diego.santacruz@epfl.ch>
2001-08-08 13:18:49 +0000

    - New dist_surf_surf() function to calculate surface to surface distance   (the functionality was previously in main() of viewer.cpp) - New struct dist_surf_surf_stats to return overall error metrics and   statistics produced by dist_surf_surf(). - New struct face_error to store per face error metrics. - New struct triag_sample_error to store the error in sampled triangles. - Added area field to struct triangle_list. - Modifed error_stat_triag to calculate other statistics (min, max and ms)

--- compute_error.h
+++ compute_error.h
@@ -1,4 +1,4 @@
-/* $Id: compute_error.h,v 1.6 2001/08/07 15:16:53 dsanta Exp $ */
+/* $Id: compute_error.h,v 1.7 2001/08/08 13:18:49 dsanta Exp $ */
 #include <3dmodel.h>
 #include <geomutils.h>
 
@@ -41,10 +41,50 @@ struct face_list {
   int n_faces; /* Number of faces in the array */
 };
 
+/* Per face error metrics */
+struct face_error {
+  double face_area;      /* Area of the face, for error weighting in averages */
+  double min_error;      /* The minimum error for the face */
+  double max_error;      /* The maximum error for the face */
+  double mean_error;     /* The mean error for the face */
+  double mean_sqr_error; /* The mean squared error for the face */
+};
+
+/* Statistics from the dist_surf_surf function */
+struct dist_surf_surf_stats {
+  double m1_area;   /* Area of model 1 surface */
+  double m2_area;   /* Area of model 2 surface */
+  double min_dist;  /* Minimum distance from model 1 to model 2 */
+  double max_dist;  /* Maximum distance from model 1 to model 2 */
+  double mean_dist; /* Mean distance from model 1 to model 2 */
+  double rms_dist;  /* Root mean squared distance from model 1 to model 2 */
+  double cell_sz;   /* The partitioning cubic cell side length */
+  struct size3d grid_sz; /* The number of cells in the partitioning grid in
+                          * each direction X,Y,Z */
+};
+
+/* Storage for triangle sample errors. */
+struct triag_sample_error {
+  double **err;      /* Error array with 2D addressing. Sample (i,j) has the
+                      * error stored at err[i][j], where i varies betwen 0 and
+                      * n_samples-1 inclusive and j varies between 0 and
+                      * n_samples-i-1 inclusive. */
+  int n_samples;     /* The number of samples in each triangle direction */
+  double *err_lin;   /* Error array with 1D adressing, which varies from 0 to
+                      * n_samples_tot-1 inclusive. It refers to the same
+                      * location as err, thus any change to err is reflected
+                      * in err_lin and vice-versa. The order in the 1D array
+                      * is all errors for i equal 0 and j from 0 to
+                      * n_samples-1, followed by errors for i equal 1 and j
+                      * from 1 to n_samples-2, and so on. */
+  int n_samples_tot; /* The total number of samples in the triangle */
+};
+
 /* A list of triangles with their associated information */
 struct triangle_list {
-  int n_triangles;                 /* The number of triangles */
   struct triangle_info *triangles; /* The triangles */
+  int n_triangles;                 /* The number of triangles */
+  double area;                     /* The total triangle area */
 };
 
 /* A triangle and useful associated information. If a vertex of the triangle
@@ -98,9 +138,14 @@ double dist_pt_surf(vertex p, const struct triangle_list *tl,
                     int **faces_in_cell, struct size3d grid_sz,
                     double cell_sz, vertex bbox_min);
 
-double error_stat_triag(double **mem_err, int n);
+void error_stat_triag(const struct triag_sample_error *tse,
+                      struct face_error *fe);
 
 struct face_list *faces_of_vertex(model *m);
+
+void dist_surf_surf(const model *m1, const model *m2, int n_spt,
+                    struct face_error *fe_ptr[],
+                    struct dist_surf_surf_stats *stats, int quiet);
 
 END_DECL
 #undef END_DECL
