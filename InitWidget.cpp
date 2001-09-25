ef2a0a9929256b6f030865308e4ae485ca96c26e
Diego Santa Cruz <diego.santacruz@epfl.ch>
2001-09-25 13:24:39 +0000

    - Cleaned up header - Initialize dialog with passed in defaults - Do proper checking of arguments in dialog - Start processing from within here instead of having to call   QApplication.exec() more than once. This apparently fixes the Win32 mutex   unlocking problem).

--- InitWidget.cpp
+++ InitWidget.cpp
@@ -1,8 +1,20 @@
-/* $Id: InitWidget.cpp,v 1.5 2001/09/20 16:46:46 dsanta Exp $ */
-#include <InitWidget.h>
-#include <qcheckbox.h>
+/* $Id: InitWidget.cpp,v 1.6 2001/09/25 13:24:39 dsanta Exp $ */
 
-InitWidget::InitWidget(QWidget *parent, const char *name):
+#include <InitWidget.h>
+
+#include <qlabel.h>
+#include <qlayout.h>
+#include <qpushbutton.h>
+#include <qfiledialog.h>
+#include <qhbox.h>
+#include <qmessagebox.h>
+#include <qvalidator.h>
+#include <qcheckbox.h>
+#include <qtimer.h>
+
+#include <stdlib.h>
+
+InitWidget::InitWidget(struct args defArgs, QWidget *parent, const char *name):
   QWidget( parent, name) {
 
   QLabel *qlabMesh1, *qlabMesh2, *qlabSplStep;
@@ -12,21 +24,22 @@ InitWidget::InitWidget(QWidget *parent, const char *name):
   QHBoxLayout *smallGrid1, *smallGrid2, *smallGrid3, *smallGrid4, *smallGrid5;
 
   /* Initialize */
-  isValid = FALSE;
+  pargs = defArgs;
 
   /* First mesh */
-  qledMesh1 = new QLineEdit("mesh1.raw", this);
+  qledMesh1 = new QLineEdit("", this);
   qlabMesh1 = new QLabel(qledMesh1, "First Mesh", this);
   B1 = new QPushButton("Browse...",this);
   connect(B1, SIGNAL(clicked()), this, SLOT(loadMesh1()));
   /* Second mesh */
-  qledMesh2 = new QLineEdit("mesh2.raw", this);
+  qledMesh2 = new QLineEdit("", this);
   qlabMesh2 = new QLabel(qledMesh2, "Second Mesh", this);
   B2 = new QPushButton("Browse...", this);
   connect(B2, SIGNAL(clicked()), this, SLOT(loadMesh2()));
   
   /* Sampling step */
-  qledSplStep = new QLineEdit("sampling step", this);
+  qledSplStep = new QLineEdit(QString("%1").arg(pargs.sampling_step*100), this);
+  qledSplStep->setValidator(new QDoubleValidator(1e-3,1e10,10,0));
   qlboxSplStep = new QListBox(this);
   qlabSplStep = new QLabel(qlboxSplStep, "Sampling step (%)", this);
   qlboxSplStep->insertItem("2");
@@ -40,51 +53,43 @@ InitWidget::InitWidget(QWidget *parent, const char *name):
 	  qledSplStep, SLOT(setText(const QString&)));
 
   /* Symmetric distance checkbox */
-  chkSymDist = new QCheckBox("Calculte the symmetric distance (double run)",
+  chkSymDist = new QCheckBox("Calculate the symmetric distance (double run)",
                              this);
+  chkSymDist->setChecked(pargs.do_symmetric);
 
   /* OK button */
   OK = new QPushButton("OK",this);
   connect(OK, SIGNAL(clicked()), this, SLOT(getParameters()));
-  connect(this, SIGNAL(exit()), qApp, SLOT(closeAllWindows()));
 
   /* Build the topmost grid layout */
   bigGrid = new QGridLayout( this, 5, 1, 20 );
 
   /* Build the grid layout for 1st mesh */
-  smallGrid1 = new QHBoxLayout();
+  smallGrid1 = new QHBoxLayout(bigGrid);
   smallGrid1->addWidget(qlabMesh1, 0, 0);
   smallGrid1->addWidget(qledMesh1, 0, 1);
   smallGrid1->addWidget(B1, 0, 2);
 
   /* Build the grid layout for 2nd mesh */
-  smallGrid2 = new QHBoxLayout();
+  smallGrid2 = new QHBoxLayout(bigGrid);
   smallGrid2->addWidget(qlabMesh2, 0, 0);
   smallGrid2->addWidget(qledMesh2, 0, 1);
   smallGrid2->addWidget(B2, 0, 2);
 
   /* Build the grid layout for sampling freq */
-  smallGrid3 = new QHBoxLayout();
+  smallGrid3 = new QHBoxLayout(bigGrid);
   smallGrid3->addWidget(qlabSplStep, 0, 0);
   smallGrid3->addWidget(qledSplStep, 0, 1);
   smallGrid3->addWidget(qlboxSplStep, 0, 2);
 
   /* Build grid layout for symmetric distance checkbox */
-  smallGrid5 = new QHBoxLayout();
+  smallGrid5 = new QHBoxLayout(bigGrid);
   smallGrid5->addWidget(chkSymDist);
 
   /* Build grid layout fir OK button */
-  smallGrid4 = new QHBoxLayout();
+  smallGrid4 = new QHBoxLayout(bigGrid);
   smallGrid4->addSpacing(100);
   smallGrid4->addWidget(OK, 0, 1);
-
-  /* Put the sub-grid layouts into the topmost one */
-  bigGrid->addLayout(smallGrid1, 0, 0);
-  bigGrid->addLayout(smallGrid2, 1, 0);
-  bigGrid->addLayout(smallGrid3, 2, 0);
-  bigGrid->addLayout(smallGrid5, 3, 0);
-  bigGrid->addLayout(smallGrid4, 4, 0);
- 
 }
 
 void InitWidget::loadMesh1() {
@@ -101,30 +106,35 @@ void InitWidget::loadMesh2() {
 }
 
 void InitWidget::getParameters() {
-  QString tmpMesh1, tmpMesh2, tmpSplStep;
+  QString str;
+  int pos;
 
-  tmpMesh1 = qledMesh1->text();
-  mesh1 = (char *)tmpMesh1.latin1();
-
-  tmpMesh2 = qledMesh2->text();
-  mesh2 = (char*)tmpMesh2.latin1();
-
-  tmpSplStep = qledSplStep->text();
-  step = (char*)tmpSplStep.latin1();
-
-  doSymmetric = chkSymDist->isChecked();
-
-  if(tmpMesh1=="mesh1.raw" || tmpMesh2=="mesh2.raw" || 
-     tmpSplStep=="sampling step")
+  str = qledSplStep->text();
+  if (qledMesh1->text().isEmpty() || qledMesh2->text().isEmpty() ||
+      qledSplStep->validator()->validate(str,pos) !=
+      QValidator::Acceptable) {
     incompleteFields();
-  else {
-    isValid = TRUE;
-    emit(exit());
+  } else {
+    // Use a timer, so that the current events acan be processed before
+    // starting the compute intensive work.
+    QTimer::singleShot(0,this,SLOT(meshRun()));
+    hide();
   }
   
 }
   
 void InitWidget::incompleteFields() {
   QMessageBox::about(this,"ERROR",
-		     "You must complete all the fields !");
+		     "Incomplete or invalid values in fields\n"
+                     "Please correct");
+}
+
+void InitWidget::meshRun() {
+  pargs.m1_fname = (char *) qledMesh1->text().latin1();
+  pargs.m2_fname = (char *) qledMesh2->text().latin1();
+  pargs.sampling_step = atof((char*)qledSplStep->text().latin1())/100;
+  pargs.do_symmetric = chkSymDist->isChecked() == TRUE;
+  mesh_run(&pargs,&model1,&model2);
+  c = new ScreenWidget(&model1, &model2);
+  c->show();
 }
--- InitWidget.h
+++ InitWidget.h
@@ -1,40 +1,34 @@
-/* $Id: InitWidget.h,v 1.5 2001/09/20 16:46:35 dsanta Exp $ */
+/* $Id: InitWidget.h,v 1.6 2001/09/25 13:24:39 dsanta Exp $ */
 
 #ifndef INITW_H
 #define INITW_H
 
-#include <qlabel.h>
-#include <qapplication.h>
-#include <qlayout.h>
+#include <qwidget.h>
 #include <qlineedit.h>
-#include <qpushbutton.h>
-#include <qfiledialog.h>
-#include <qhbox.h>
-#include <qmessagebox.h>
 #include <qcheckbox.h>
+#include <mesh_run.h>
+#include <compute_error.h>
+#include <ScreenWidget.h>
 
 class InitWidget : public QWidget {
   Q_OBJECT
 
 public:
-  InitWidget(QWidget *parent=0, const char *name=0 );
-  char *mesh1, *mesh2, *step;
-  bool isValid;
-  bool doSymmetric;
+  InitWidget(struct args defArgs, QWidget *parent=0, const char *name=0 );
 
-public slots:
-    void loadMesh1();
-    void loadMesh2();
-    void getParameters();
-    void incompleteFields();
-    
-signals:
-    void exit();
+private slots:
+  void loadMesh1();
+  void loadMesh2();
+  void getParameters();
+  void incompleteFields();
+  void meshRun();
 
 private:
     QLineEdit *qledMesh1, *qledMesh2, *qledSplStep; 
     QCheckBox *chkSymDist;
-
+    struct args pargs;
+    struct model_error model1,model2;
+    ScreenWidget *c;
 };
 
 #endif
