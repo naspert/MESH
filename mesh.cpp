350dcb4abd3c66802c5bdf285226552dd9b595d8
Diego Santa Cruz <diego.santacruz@epfl.ch>
2001-09-25 13:16:19 +0000

    - Moved main processing function from mesh.cpp to mesh_run.[ch] - Made main processing function independent of GUI. - Do not call QApplication.exec() more than once (apparently fixes   Win32 mutex unlocking problem) - Cleaned up includes

--- mesh.cpp
+++ mesh.cpp
@@ -1,27 +1,13 @@
-/* $Id: mesh.cpp,v 1.4 2001/09/20 16:46:56 dsanta Exp $ */
+/* $Id: mesh.cpp,v 1.5 2001/09/25 13:16:19 dsanta Exp $ */
 
 #include <time.h>
 #include <string.h>
 #include <qapplication.h>
-#include <qevent.h>
-#include <qobject.h>
-#include <qkeycode.h>
 
-#include <compute_error.h>
-#include <model_analysis.h>
 #include <ScreenWidget.h>
 #include <InitWidget.h>
 
-/* To store the parsed arguments */
-struct args {
-  char *m1_fname; /* filename of model 1 */
-  char *m2_fname; /* filename of model 2 */
-  int  no_gui;    /* text only flag */
-  int quiet;      /* do not display extra info flag*/
-  double sampling_step; /* The sampling step, as fraction of the bounding box
-                         * diagonal of model 2. */
-  int do_symmetric; /* do symmetric error measure */
-};
+#include <mesh_run.h>
 
 /* Prints usage information to the out stream */
 static void print_usage(FILE *out)
@@ -122,20 +108,13 @@ static void parse_args(int argc, char **argv, struct args *pargs)
 
 int main( int argc, char **argv )
 {
-  clock_t start_time;
-  model_error model1,model2;
-  struct face_list *vfl;
   int i;
   QString m1,n1,o1;
-  struct face_error *fe = NULL;
-  struct face_error *fe_rev = NULL;
-  struct dist_surf_surf_stats stats;
-  struct dist_surf_surf_stats stats_rev;
-  double bbox1_diag,bbox2_diag;
   struct args pargs;
-  ScreenWidget *c;
   QApplication *a;
-  struct model_info m1info,m2info;
+  InitWidget *b;
+  ScreenWidget *c;
+  struct model_error model1,model2;
 
   /* Initialize application */
   i = 0;
@@ -146,6 +125,7 @@ int main( int argc, char **argv )
   }
   if (i == argc) { /* no text version requested, initialize QT */
     a = new QApplication( argc, argv );
+    if (a != NULL) a->connect( a, SIGNAL(lastWindowClosed()), a, SLOT(quit()) );
   } else {
     a = NULL; /* No QT app needed */
   }
@@ -153,193 +133,23 @@ int main( int argc, char **argv )
   /* Parse arguments */
   parse_args(argc,argv,&pargs);
   /* Display starting dialog if insufficient arguments */
-  if (argc > 1) {
+  if (pargs.m1_fname != NULL || pargs.m2_fname != NULL) {
     if (pargs.m1_fname == NULL || pargs.m2_fname == NULL) {
       fprintf(stderr,"ERROR: missing file name(s) in command line\n");
       exit(1);
     }
+    mesh_run(&pargs,&model1,&model2);
   } else {
-    InitWidget *b;
-    b = new InitWidget() ;
-    a->setMainWidget(b);
+    b = new InitWidget(pargs);
     b->show(); 
-    a->exec();
-
-    if (!b->isValid) exit(1);
-    pargs.m1_fname = b->mesh1;
-    pargs.m2_fname = b->mesh2;
-    pargs.sampling_step = atof(b->step)/100;
-    pargs.do_symmetric = b->doSymmetric == TRUE;
   }
-  
-  /* Read models from input files */
-  memset(&model1,0,sizeof(model1));
-  memset(&model2,0,sizeof(model2));
-  model1.mesh = read_raw_model(pargs.m1_fname);
-  model2.mesh = read_raw_model(pargs.m2_fname);
-
-  /* Analyze models (we don't need normals for model 1, so we don't request
-   * for it to be oriented). */
-  start_time = clock();
-  bbox1_diag = dist(model1.mesh->bBox[0], model1.mesh->bBox[1]);
-  bbox2_diag = dist(model2.mesh->bBox[0], model2.mesh->bBox[1]);
-  vfl = faces_of_vertex(model1.mesh);
-  analyze_model(model1.mesh,vfl,&m1info,0);
-  model1.info = &m1info;
-  analyze_model(model2.mesh,NULL,&m2info,1);
-  model2.info = &m2info;
-  if(pargs.no_gui){
-    free_face_lists(vfl,model1.mesh->num_vert);
-    vfl = NULL;
-  }
-  /* Adjust sampling step size */
-  pargs.sampling_step *= bbox2_diag;
-
-  /* Print available model information */
-  printf("\n                      Model information\n\n");
-  printf("Number of vertices:     \t%11d\t%11d\n",
-         model1.mesh->num_vert,model2.mesh->num_vert);
-  printf("Number of triangles:    \t%11d\t%11d\n",
-         model1.mesh->num_faces,model2.mesh->num_faces);
-  printf("BoundingBox diagonal:   \t%11g\t%11g\n",
-         bbox1_diag,bbox2_diag);
-  printf("Number of disjoint parts:\t%11d\t%11d\n",
-         m1info.n_disjoint_parts,m2info.n_disjoint_parts);
-  printf("Manifold:               \t%11s\t%11s\n",
-         (m1info.manifold ? "yes" : "no"), (m2info.manifold ? "yes" : "no"));
-  printf("Originally oriented:    \t%11s\t%11s\n",
-         (m1info.orig_oriented ? "yes" : "no"),
-         (m2info.orig_oriented ? "yes" : "no"));
-  printf("Orientable:             \t%11s\t%11s\n",
-         (m1info.orientable ? "yes" : "no"),
-         (m2info.orientable ? "yes" : "no"));
-  printf("Closed:                 \t%11s\t%11s\n",
-         (m1info.closed ? "yes" : "no"),
-         (m2info.closed ? "yes" : "no"));
-  fflush(stdout);
-
-  /* Compute the distance from one model to the other */
-  dist_surf_surf(model1.mesh,model2.mesh,pargs.sampling_step,
-                 &fe,&stats,!pargs.no_gui,pargs.quiet);
-
-  /* Print results */
-  printf("Surface area:           \t%11g\t%11g\n",
-         stats.m1_area,stats.m2_area);
-  printf("\n       Distance from model 1 to model 2\n\n");
-  printf("        \t   Absolute\t%% BBox diag\n");
-  printf("        \t           \t  (Model 2)\n");
-  printf("Min:    \t%11g\t%11g\n",
-         stats.min_dist,stats.min_dist/bbox2_diag*100);
-  printf("Max:    \t%11g\t%11g\n",
-         stats.max_dist,stats.max_dist/bbox2_diag*100);
-  printf("Mean:   \t%11g\t%11g\n",
-         stats.mean_dist,stats.mean_dist/bbox2_diag*100);
-  printf("RMS:    \t%11g\t%11g\n",
-         stats.rms_dist,stats.rms_dist/bbox2_diag*100);
-  printf("\n");
-  fflush(stdout);
-
-  if (pargs.do_symmetric) { /* Invert models and recompute distance */
-    printf("       Distance from model 2 to model 1\n\n");
-    dist_surf_surf(model2.mesh,model1.mesh,pargs.sampling_step,
-                   &fe_rev,&stats_rev,0,pargs.quiet);
-    free_face_error(fe_rev);
-    fe_rev = NULL;
-    printf("        \t   Absolute\t%% BBox diag\n");
-    printf("        \t           \t  (Model 2)\n");
-    printf("Min:    \t%11g\t%11g\n",
-           stats_rev.min_dist,stats_rev.min_dist/bbox2_diag*100);
-    printf("Max:    \t%11g\t%11g\n",
-           stats_rev.max_dist,stats_rev.max_dist/bbox2_diag*100);
-    printf("Mean:   \t%11g\t%11g\n",
-           stats_rev.mean_dist,stats_rev.mean_dist/bbox2_diag*100);
-    printf("RMS:    \t%11g\t%11g\n",
-           stats_rev.rms_dist,stats_rev.rms_dist/bbox2_diag*100);
-    printf("\n");
-
-    /* Print symmetric distance measures */
-    printf("       Symmetric distance between model 1 and model 2\n\n");
-    printf("        \t   Absolute\t%% BBox diag\n");
-    printf("        \t           \t  (Model 2)\n");
-    printf("Min:    \t%11g\t%11g\n",
-           max(stats.min_dist,stats_rev.min_dist),
-           max(stats.min_dist,stats_rev.min_dist)/bbox2_diag*100);
-    printf("Max:    \t%11g\t%11g\n",
-           max(stats.max_dist,stats_rev.max_dist),
-           max(stats.max_dist,stats_rev.max_dist)/bbox2_diag*100);
-    printf("Mean:   \t%11g\t%11g\n",
-           max(stats.mean_dist,stats_rev.mean_dist),
-           max(stats.mean_dist,stats_rev.mean_dist)/bbox2_diag*100);
-    printf("RMS:    \t%11g\t%11g\n",
-           max(stats.rms_dist,stats_rev.rms_dist),
-           max(stats.rms_dist,stats_rev.rms_dist)/bbox2_diag*100);
-    printf("\n");
-  }
-
-  printf("               \t       Absolute\t   %% BBox diag model 2\n");
-  printf("Sampling step: \t%15g\t%22g\n",pargs.sampling_step,
-         pargs.sampling_step/bbox2_diag*100);
-  printf("\n");
-  if (!pargs.do_symmetric) {
-    printf("        \t    Total\tAvg. / triangle\t"
-           "      Avg. / triangle\n"
-           "        \t         \t     of model 1\t"
-           "           of model 2\n");
-    printf("Samples:\t%9d\t%15.2f\t%21.2f\n",
-           stats.m1_samples,((double)stats.m1_samples)/model1.mesh->num_faces,
-           ((double)stats.m1_samples)/model2.mesh->num_faces);
-  } else {
-    printf("                 \t    Total\tAvg. / triangle\t"
-           "      Avg. / triangle\n"
-           "                 \t         \t     of model 1\t"
-           "           of model 2\n");
-    printf("Samples (1 to 2):\t%9d\t%15.2f\t%21.2f\n",
-           stats.m1_samples,((double)stats.m1_samples)/model1.mesh->num_faces,
-           ((double)stats.m1_samples)/model2.mesh->num_faces);
-    printf("Samples (2 to 1):\t%9d\t%15.2f\t%21.2f\n",
-           stats_rev.m1_samples,
-           ((double)stats_rev.m1_samples)/model1.mesh->num_faces,
-           ((double)stats_rev.m1_samples)/model2.mesh->num_faces);
-  }
-  printf("\n");
-  if (!pargs.do_symmetric) {
-    printf("                       \t     X\t    Y\t   Z\t   Total\n");
-    printf("Partitioning grid size:\t%6d\t%5d\t%4d\t%8d\n",
-           stats.grid_sz.x,stats.grid_sz.y,stats.grid_sz.z,
-           stats.grid_sz.x*stats.grid_sz.y*stats.grid_sz.z);
-  } else {
-    printf("                                \t     X\t    Y\t   Z\t   Total\n");
-    printf("Partitioning grid size (1 to 2):\t%6d\t%5d\t%4d\t%8d\n",
-           stats.grid_sz.x,stats.grid_sz.y,stats.grid_sz.z,
-           stats.grid_sz.x*stats.grid_sz.y*stats.grid_sz.z);
-    printf("Partitioning grid size (2 to 1):\t%6d\t%5d\t%4d\t%8d\n",
-           stats_rev.grid_sz.x,stats_rev.grid_sz.y,stats_rev.grid_sz.z,
-           stats_rev.grid_sz.x*stats_rev.grid_sz.y*stats_rev.grid_sz.z);
-  }
-  printf("\n");
-  printf("Execution time (secs.):\t%.2f\n",
-         (double)(clock()-start_time)/CLOCKS_PER_SEC);
-  fflush(stdout);
-
-  if(pargs.no_gui){
-    free_face_error(fe);
-    fe = NULL;
-    return 0;
-  } else {
-    /* Get the per vertex error metric */
-    calc_vertex_error(&model1,fe,vfl);
-    /* Free now useless data */
-    free_face_error(fe);
-    fe = NULL;
-    free_face_lists(vfl,model1.mesh->num_vert);
-    vfl = NULL;
-
-   
-    c = new ScreenWidget(&model1, &model2);
-    QObject::connect(c->quitBut, SIGNAL(clicked()), 
-                     a, SLOT(quit()));
-    a->setMainWidget( c );
-    c->show(); 
+  if (a != NULL) {
+    if (pargs.m1_fname != NULL || pargs.m2_fname != NULL) {
+      c = new ScreenWidget(&model1, &model2);
+      c->show(); 
+    }
     return a->exec();
+  } else {
+    return 0;
   }
 }
