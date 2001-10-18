413343507acf82ec2ca0ac7d2518d4b6ea4bd668
Nicolas Aspert <nicolas.aspert@epfl.ch>
2001-10-18 16:23:40 +0000

    Declaration of the crease version of the Butterfly subdivision scheme

--- lib3d/include/subdiv_methods.h
+++ lib3d/include/subdiv_methods.h
@@ -1,4 +1,4 @@
-/* $Id: subdiv_methods.h,v 1.2 2001/10/16 15:31:05 aspert Exp $ */
+/* $Id: subdiv_methods.h,v 1.3 2001/10/18 16:23:40 aspert Exp $ */
 
 
 #ifndef __SUBDIV_METHODS_PROTO
@@ -10,10 +10,15 @@
 # define U2_FOUND 0x04
 
 /* Identifier for subdivision method */
-# define SUBDIV_BUTTERFLY 0x01
-# define SUBDIV_SPH       0x02
-# define SUBDIV_LOOP      0x03
-# define SUBDIV_LOOP_BOUNDARY 0x04
+# define SUBDIV_BUTTERFLY          0x01
+# define SUBDIV_SPH                0x02
+# define SUBDIV_LOOP               0x03
+# define SUBDIV_BUTTERFLY_BOUNDARY 0x11
+# define SUBDIV_LOOP_BOUNDARY      0x13
+
+/* This is a parameter for Butterfly subdivision */
+static const double w=-0.0625; 
+
 
 # ifdef __cplusplus
 extern "C" {
@@ -26,6 +31,8 @@ extern "C" {
   void compute_midpoint_loop(struct ring_info*, int, int, struct model*,
 			     vertex_t*);
   void update_vertices_loop(struct model*, struct model*, struct ring_info*);
+  void compute_midpoint_butterfly_crease(struct ring_info*, int, int, 
+					 struct model*, vertex_t*);
   void compute_midpoint_loop_crease(struct ring_info*, int, int, struct model*,
 				    vertex_t*);
   void update_vertices_loop_crease(struct model*, struct model*, 
