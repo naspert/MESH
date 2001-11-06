638382ba0807bef462a922b57495291b08b17efc
Diego Santa Cruz <diego.santacruz@epfl.ch>
2001-11-06 10:39:18 +0000

    - Reimplemented window based log: use memory buffer instead of pipe (no   dealock possible); show window contents as they are written. - Use better font settings.

--- TextWidget.cpp
+++ TextWidget.cpp
@@ -1,67 +1,40 @@
 #include <TextWidget.h>
+#include <qapplication.h>
 
 TextWidget::TextWidget(QWidget *parent, const char *name)
   : QWidget(parent, name) {
-  
+  QFont font(QApplication::font());
+
   layout = new QVBoxLayout(this);
   layout->setResizeMode(QLayout::FreeResize);
   view = new QTextView(this);
-  view->setMinimumSize(600, 600);
   view->setTextFormat(Qt::PlainText);
-  view->setFont(QFont("courier", 9));
+  font.setFamily("courier");
+  view->setFont(font);
   layout->addWidget(view);
   butClose = new QPushButton("Close", this);
-  butClose->setMinimumSize(40, 20);
-  butClose->setMaximumSize(40, 20);
+  butClose->setSizePolicy(QSizePolicy(QSizePolicy::Maximum,
+                                      QSizePolicy::Minimum));
   layout->addWidget(butClose);
 
   connect(butClose, SIGNAL(clicked()), this, SLOT(close()));
 
 }
 
-void TextWidget::openFile(FILE *in_stream) {
-  int readChar;
-  QString buf;
-
-  qfIn.open(IO_ReadOnly, in_stream);
-  while (!qfIn.atEnd()) {
-    readChar = qfIn.readLine(buf, 500);
-    if (!buf.isEmpty())
-      text += buf;
-  }
-  qfIn.close();
-  view->setText(text);
+QSize TextWidget::sizeHint() const {
+  return QSize(550,500);
 }
 
-// This one is a shortcut that merges the previous ones
-TextWidget::TextWidget(FILE *in_stream, QWidget *parent, const char *name)
-  : QWidget(parent, name) {
-
-  QString buf;
-  int readChar;
-
-
-  qfIn.open(IO_ReadOnly, in_stream);
-  layout = new QVBoxLayout(this);
-  layout->setResizeMode(QLayout::FreeResize);
-  view = new QTextView(this);
-  view->setMinimumSize(600, 600);
-  view->setTextFormat(Qt::PlainText);
-  view->setFont(QFont("courier", 9));
-  layout->addWidget(view);
-  butClose = new QPushButton("Close", this);
-  butClose->setMinimumSize(40, 20);
-  butClose->setMaximumSize(40, 20);
-  layout->addWidget(butClose);
-
-  connect(butClose, SIGNAL(clicked()), this, SLOT(close()));
-
-  while (!qfIn.atEnd()) {
-    readChar = qfIn.readLine(buf, 500);
-    if (!buf.isEmpty())
-      text += buf;
-  }
-  qfIn.close();
-  view->setText(text);
+void TextWidget::append(const QString &str) {
+  // The append() method of QT's TextView is buggy. Use the recommended
+  // workaround.
+  view->setText(view->text()+str);
+  qApp->processEvents(100);
 }
 
+void TextWidget_puts(void *out, const char *str) {
+  TextWidget *tw;
+
+  tw = (TextWidget*) out;
+  tw->append(QString(str));
+}
--- TextWidget.h
+++ TextWidget.h
@@ -1,26 +1,25 @@
-#include <mesh_run.h>
 #include <qpushbutton.h>
 #include <qtextview.h>
 #include <qlayout.h>
-#include <qfile.h>
 
 #ifndef _TEXTWIDGET_PROTO
 #define _TEXTWIDGET_PROTO
+
+extern "C" {
+  void TextWidget_puts(void *out, const char *str);
+}
 
 class TextWidget : public QWidget {
 
 public:
   TextWidget(QWidget *parent=0, const char *name=0);
-  void openFile(FILE *in_stream);
-  TextWidget(FILE *in_stream, QWidget *parent=0, const char *name=0);
+  void append(const QString &str);
+  QSize sizeHint() const;
 
 
 private:
   QVBoxLayout *layout;
   QTextView *view;
   QPushButton *butClose;
-  QString text;
-  QFile qfIn;
-
 };
 #endif
