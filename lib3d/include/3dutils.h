176df93b036cc0956f1c38d95d62f7a5b8082b9c
Nicolas Aspert <nicolas.aspert@epfl.ch>
2002-02-05 09:02:18 +0000

    - Now uses the 'model_in' routines to read VRML/Raw files

--- lib3d/include/3dutils.h
+++ lib3d/include/3dutils.h
@@ -1,6 +1,7 @@
-/* $Id: 3dutils.h,v 1.1 2001/03/12 14:50:32 aspert Exp $ */
+/* $Id: 3dutils.h,v 1.2 2002/02/05 09:02:18 aspert Exp $ */
 #include <3dmodel.h>
 #include <normals.h>
+#include <model_in.h>
 #include <3dmodel_io.h>
 #include <geomutils.h>
 
