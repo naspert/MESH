02a36747eea3519df3624d83379d454d203884a8
Nicolas Aspert <nicolas.aspert@epfl.ch>
2002-05-13 14:47:41 +0000

    - Added a (basic) copyright s.t. those files can go to Eurecom guys

--- lib3d/include/model_out.h
+++ lib3d/include/model_out.h
@@ -1,4 +1,29 @@
-/* $Id: model_out.h,v 1.1 2002/05/13 08:38:50 aspert Exp $ */
+/* $Id: model_out.h,v 1.2 2002/05/13 14:47:41 aspert Exp $ */
+
+
+/*
+ *
+ *  Copyright (C) 2001-2002 EPFL (Swiss Federal Institute of Technology,
+ *  Lausanne)
+ *  You are not allowed to redistribute this program without the explicit
+ *  permission of the author.
+ *
+ *  Author : Nicolas Aspert
+ *
+ *  Contact : 
+ *     Nicolas Aspert
+ *     Signal Processing Institute (ITS)
+ *     Swiss Federal Institute of Technology (EPFL)
+ *     1015 Lausanne
+ *     Switzerland
+ *
+ *     Tel : +41 21 693 3632
+ *     E-Mail : Nicolas.Aspert@epfl.ch
+ *
+ *
+ */
+
+
 
 #ifndef _MODEL_OUT_PROTO
 #define _MODEL_OUT_PROTO
--- lib3d/include/ring.h
+++ lib3d/include/ring.h
@@ -1,4 +1,29 @@
-/* $Id: ring.h,v 1.1 2002/05/13 13:50:46 aspert Exp $ */
+/* $Id: ring.h,v 1.2 2002/05/13 14:47:41 aspert Exp $ */
+
+
+/*
+ *
+ *  Copyright (C) 2001-2002 EPFL (Swiss Federal Institute of Technology,
+ *  Lausanne)
+ *  You are not allowed to redistribute this program without the explicit
+ *  permission of the author.
+ *
+ *  Author : Nicolas Aspert
+ *
+ *  Contact : 
+ *     Nicolas Aspert
+ *     Signal Processing Institute (ITS)
+ *     Swiss Federal Institute of Technology (EPFL)
+ *     1015 Lausanne
+ *     Switzerland
+ *
+ *     Tel : +41 21 693 3632
+ *     E-Mail : Nicolas.Aspert@epfl.ch
+ *
+ *
+ */
+
+
 #include <3dmodel.h>
 
 #ifndef _RING_PROTO
