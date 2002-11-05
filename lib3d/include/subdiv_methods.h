7d55e4d77db56c276706e134e5c39d12b8e2d4ad
Nicolas Aspert <nicolas.aspert@epfl.ch>
2002-11-05 13:34:45 +0000

    - Removed bitmasks no longer used

--- lib3d/include/subdiv_methods.h
+++ lib3d/include/subdiv_methods.h
@@ -1,14 +1,10 @@
-/* $Id: subdiv_methods.h,v 1.15 2002/11/01 12:50:01 aspert Exp $ */
+/* $Id: subdiv_methods.h,v 1.16 2002/11/05 13:34:45 aspert Exp $ */
 #include <3dmodel.h>
 #include <ring.h>
 
 #ifndef __SUBDIV_METHODS_PROTO
 # define __SUBDIV_METHODS_PROTO
 
-/* Bitmasks for the midpoint search */
-# define U0_FOUND 0x01
-# define U1_FOUND 0x02
-# define U2_FOUND 0x04
 
 /* Identifier for subdivision method */
 # define SUBDIV_BUTTERFLY          0x01
